0379a595096882bf844a48cd02bdf83a7f90f12b
Taullo <me@taullo.com>
2023-07-04 00:33:47 -0500

    Fix maxId missing from community firehose

--- app/javascript/flavours/aether/features/firehose/index.jsx
+++ app/javascript/flavours/aether/features/firehose/index.jsx
@@ -107,7 +107,7 @@ const Firehose = ({ feedType, multiColumn }) => {
     (maxId) => {
       switch(feedType) {
       case 'community':
-        dispatch(expandCommunityTimeline({ onlyMedia }));
+        dispatch(expandCommunityTimeline({ maxId, onlyMedia }));
         break;
       case 'public':
         dispatch(expandPublicTimeline({ maxId, onlyMedia, allowLocalOnly }));
