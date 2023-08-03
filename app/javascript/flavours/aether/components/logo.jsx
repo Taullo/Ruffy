c7718270f71e121952990a5b34e3ad6a2496f1c2
Taullo <me@taullo.com>
2023-08-02 20:12:07 -0500

    Change logo.jsx to remove mastodon title

--- app/javascript/flavours/aether/components/logo.jsx
+++ app/javascript/flavours/aether/components/logo.jsx
@@ -2,13 +2,13 @@ import logo from 'mastodon/../images/logo.svg';
 
 export const WordmarkLogo = () => (
   <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
+    <title>Ruffy</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
 );
 
 export const SymbolLogo = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src={logo} alt='Ruffy' className='logo logo--icon' />
 );
 
 export default WordmarkLogo;
