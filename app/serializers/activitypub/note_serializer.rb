a5bf06ec6ba80642bbd9bc151a2273ebfb86b3d3
Taullo <me@taullo.com>
2023-08-06 20:13:30 -0500

    Fix typo

--- app/serializers/activitypub/note_serializer.rb
+++ app/serializers/activitypub/note_serializer.rb
@@ -117,7 +117,7 @@ class ActivityPub::NoteSerializer < ActivityPub::Serializer
   end
 
   def sensitive
-    object.account.sensitized? || object.sensitive || (!instance_options[:allow_local_only] && Setting.outgoing_spoilers.present?) || Setting.outgoing_sensitive || object.account.user&.setting_default_privacy
+    object.account.sensitized? || object.sensitive || (!instance_options[:allow_local_only] && Setting.outgoing_spoilers.present?) || Setting.outgoing_sensitive || object.account.user&.setting_default_sensitive
   end
 
   def virtual_attachments
