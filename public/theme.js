9570a998bd277858c24a8baff525f1e717d85226
Taullo <me@taullo.com>
2024-03-31 02:08:53 -0700

    Change how theme js is loaded

--- public/theme.js
+++ public/theme.js
@@ -4,11 +4,17 @@
     const prefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
     if (((prefersLight === true) && (theme === 'auto')) || (theme === 'light')) {
       document.body.classList.toggle('light-theme', true);
+      document.body.classList.toggle('dark-theme', false);
+      document.body.classList.toggle('mixed-theme', false);
     }
     else if (((prefersLight === false) && (theme === 'auto')) || (theme === 'dark')) {
+      document.body.classList.toggle('light-theme', false);
       document.body.classList.toggle('dark-theme', true);
+      document.body.classList.toggle('mixed-theme', false);
     }
     else {
+      document.body.classList.toggle('light-theme', false);
+      document.body.classList.toggle('dark-theme', false);
       document.body.classList.toggle('mixed-theme', true);
     }
   });
